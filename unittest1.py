6696119e6f494bcbb0d913457331179ec081a009
ss842 <ss842@duke.edu>
2017-09-07 16:16:28 -0400

    version 1 of unit test

---
+++ unittest1.py
@@ -0,0 +1,22 @@
+import pytest
+
+def Jawad_placeholder (input1, input2):
+
+
+
+def test1integer ():
+	assert (Jawad_placeholder(4,6),10)
+
+def test2floating ():
+	assert (Jawad_placeholder(-4,2),0)
+
+def test3string ():
+	assert (Jawad_placeholder('word',3),'value error')
+
+
+
+print("Test 1 result:" + test1integer())
+print("Test 2 result:" + test2floating())
+print("Test 3 result:" + test3string())
+
+
