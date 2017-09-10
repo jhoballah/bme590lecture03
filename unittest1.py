e152dd37c7411da27d2625ac62bc833d9861ed8e
ss842 <ss842@duke.edu>
2017-09-10 19:29:38 -0400

    ughhhh

--- unittest1.py
+++ unittest1.py
@@ -1,22 +1,28 @@
 import pytest
 
-def Jawad_placeholder (input1, input2):
+
+def test (num1, num2):
+
+def testinteger ():
+   assert (summation_func(4,6),10)
+
+def testfloating ():
+   assert (summation_func(-4,2),0)
+
+def testcomplex ():
+   assert (summation_func(3j+1, 8),9)
+
+def testargument ():
+   with pytest.raises(NotEnoughArguments):
+	   summation_func(1)
+
+print("Integer Test Result:" + testinteger())
+print("Floating Test Result:" + testfloating())
+print("Complex Test Result:" + testcomplex())
+print("String Test Result:" + teststring())
+print("Argument Test Result:" + testargument())
 
 
 
-def test1integer ():
-	assert (Jawad_placeholder(4,6),10)
-
-def test2floating ():
-	assert (Jawad_placeholder(-4,2),0)
-
-def test3string ():
-	assert (Jawad_placeholder('word',3),'NaN')
-
-
-
-print("Test 1 result:" + test1integer())
-print("Test 2 result:" + test2floating())
-print("Test 3 result:" + test3string())
 
 
